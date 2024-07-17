cf648ba63524bb4e2b562a807a718895f30723c2
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-07-17 23:21:10 +0530

    analytics added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,12 +26,15 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        
         <Link
           href={"/"}
           className="text-transparent bg-clip-text bg-gradient-to-r to-indigo-600 from-violet-400"  
         >
+          
           Made with <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-red-400">❤️</span>
         </Link>
+        
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
